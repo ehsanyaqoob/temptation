38af5bd5a3ed41bc7a7c447e83666df9ad91181f
ehsanyaqoob <ehsanyaqoob07@gmail.com>
2026-03-30 23:14:35 +0500

    Initial Commit

---
+++ temptation/temptationApp.swift
@@ -0,0 +1,17 @@
+//
+//  temptationApp.swift
+//  temptation
+//
+//  Created by ehsanyaqoob on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct temptationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
